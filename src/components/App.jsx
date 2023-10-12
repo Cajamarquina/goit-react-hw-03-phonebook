367785005d77330603716658ae445c3ad61e3078
Cajamarquina <127974997+Cajamarquina@users.noreply.github.com>
2023-10-12 11:26:18 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,70 +7,80 @@ import './App.css';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
     name: '',
     number: '',
   };
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  handleSubmit = (name, number) => {
-    if (name.trim() === '' || number.trim() === '') {
-      alert('Name and number cannot be empty');
-      return;
-    }
-
-    const isExist = this.state.contacts.some((contact) => contact.name === name);
-
-    if (isExist) {
-      alert(`${name} is already in contacts.`);
-      return;
-    }
-
-    const newContact = {
-      id: nanoid(),
-      name,
-      number,
-    };
-
-    this.setState((prevState) => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
-  };
-
-  handleDeleteContact = (id) => {
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter((contact) => contact.id !== id),
-    }));
-  };
-
-  render() {
-    const { filter, contacts } = this.state;
-
-    const filteredContacts = contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.handleSubmit} />
-        <h2>Contacts</h2>
-        <Filter filter={filter} handleChange={this.handleChange} />
-        <ContactList contacts={filteredContacts} onDeleteContact={this.handleDeleteContact} />
-      </div>
-    );
+// Add componentDidMount to load contacts from localStorage
+componentDidMount() {
+  const storedContacts = localStorage.getItem('contacts');
+  if (storedContacts) {
+    this.setState({ contacts: JSON.parse(storedContacts) });
   }
+}
+
+// Use componentDidUpdate to save contacts to localStorage when they change
+componentDidUpdate(prevProps, prevState) {
+  if (this.state.contacts !== prevState.contacts) {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+}
+
+handleChange = (e) => {
+  this.setState({
+    [e.target.name]: e.target.value,
+  });
+};
+
+handleSubmit = (name, number) => {
+  if (name.trim() === '' || number.trim() === '') {
+    alert('Name and number cannot be empty');
+    return;
+  }
+
+  const isExist = this.state.contacts.some((contact) => contact.name === name);
+
+  if (isExist) {
+    alert(`${name} is already in contacts.`);
+    return;
+  }
+
+  const newContact = {
+    id: nanoid(),
+    name,
+    number,
+  };
+
+  this.setState((prevState) => ({
+    contacts: [...prevState.contacts, newContact],
+  }));
+};
+
+handleDeleteContact = (id) => {
+  this.setState((prevState) => ({
+    contacts: prevState.contacts.filter((contact) => contact.id !== id),
+  }));
+};
+
+render() {
+  const { filter, contacts } = this.state;
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={this.handleSubmit} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} handleChange={this.handleChange} />
+      <ContactList contacts={filteredContacts} onDeleteContact={this.handleDeleteContact} />
+    </div>
+  );
+}
 }
 
 export default App;
